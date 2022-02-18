e62fef66ab03be03e4efa5c60bec6d425164c6a9
Eddiedellz <Edwardolson711@outlook.com>
2022-02-18 17:26:04 -0500

    config update

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # SMS-Notifications
 Klipper SMS Notifications through Twilio
+
+Add to config file
+[sms_notification]
+ACCSID: YOUR ACCSID
+AUTHTOKEN: YOUR AUTH KEY
+PHONETO: "+1XXXXXXXXXX"
+PHONEFROM: +1XXXXXXXXXX
+SMSMESSAGE: "+1XXXXXXXXXX" 
--- src/SMS_Alert.py
+++ src/SMS_Alert.py
@@ -1,18 +1,14 @@
 from twilio.rest import Client 
 from twilio.base.exceptions import TwilioRestException
-import os
 
-class twilioacc:
-#ACCSID: "AC0620680188289306ff143107d740e559"
-#AUTHTOKEN: "XXXXXXXXXXXXXX"
-#PHONETO: "+1XXXXXXXXXX"
-#SMSMESSAGE: "+1XXXXXXXXXX" 
+class smsnotification:
 
     def __init__(self, config):
         self.printer = config.get_printer()
         self.ACCSID = config.get('ACCSID', None)
         self.AUTHTOKEN = config.get('AUTHTOKEN', None)
         self.PHONETO = config.get('PHONETO', None)
+        self.PHONETO = config.get('PHONEFROM', None)
         self.MSG = config.get('MSG', None)
         self.printer.register_event_handler('klippy:ready', self._handle_ready)
         gcode = self.printer.lookup_object('gcode')
@@ -22,20 +18,20 @@ class twilioacc:
         self._handle_ready()
         try:
             client = Client(self.ACCSID, self.AUTHTOKEN) 
-	    client.api.account.messages.create(
-                        body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-                        from_='+18484208874',
-                        to='+17188252399'
-                 )
+            
+            message = client.messages.create(
+            to="+17188252399", 
+            from_="+18484208874",
+            body=self.MSG)
+            gcmd.respond_info(message.sid)
             
         except TwilioRestException as errf:
-            raise gcmd.error(errf, " " & self.AUTHTOKEN)
+            raise gcmd.error(errf)
 
     def _handle_ready(self):
         self.reactor = self.printer.get_reactor()
         self.printProgress = 0
-        self.displayWEStatus = self.printer.lookup_object('display_status')
+        self.displayStatus = self.printer.lookup_object('display_status')            
 
-            
 def load_config(config):
-    return twilioacc(config)
+    return smsnotification(config)
